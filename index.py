246192d56136016c30e80f6affac90c351bd54c3
iigmir <roc120j@gmail.com>
2020-09-18 01:03:39 +0800

    Released

---
+++ index.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template, jsonify
+import requests
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return render_template("f2e.html")
+
+@app.route("/api", methods=["GET"])
+@app.route("/api/<id>", methods=["GET"])
+def get_tasks(id=None):
+    # Varaibles
+    if id is None:
+        id_api = ""
+        explain = "https://www.explainxkcd.com"
+        tw = "https://xkcd.tw"
+    else:
+        id_api = id + "/"
+        explain = "https://www.explainxkcd.com/wiki/index.php/" + id
+        tw = "https://xkcd.tw/" + id
+    # Request
+    url = "https://xkcd.com/" + id_api + "info.0.json"
+    response = requests.get( url )
+    if response.status_code == 404:
+        response = requests.get( "https://xkcd.com/info.0.json" )
+    return jsonify({
+        "response": response.json(),
+        "id": id,
+        "request_url": url,
+        "explain": explain,
+        "tw": tw
+    })
